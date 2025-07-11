e82e364ab2adc3f36daa507d12ce229aefc28fce
zixphay <phamtiend77@gmail.com>
2025-07-12 01:14:39 +0700

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
